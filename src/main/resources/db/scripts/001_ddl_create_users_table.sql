69f0a48a7b882cac134666e85139b7c1e4e87e8e
Artyrio226 <artur_sar_80@mail.ru>
2024-04-02 09:08:24 +0300

    Доменная модель [#505091]

--- src/main/resources/db/scripts/001_ddl_create_users_table.sql
+++ src/main/resources/db/scripts/001_ddl_create_users_table.sql
@@ -1,7 +1,7 @@
 create table users
 (
     id        serial primary key,
-    username  varchar not null,
+    username  varchar not null unique,
     email     varchar not null unique,
     password  varchar not null
 );
