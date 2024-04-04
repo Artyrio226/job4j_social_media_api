8a45b234745cc2226ae142bd386fd85ff54eb4bf
Artyrio226 <artur_sar_80@mail.ru>
2024-04-04 10:56:14 +0300

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -2,7 +2,10 @@ package ru.job4j.socialmediaapi.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmediaapi.model.Post;
 
 import java.time.LocalDateTime;
@@ -15,4 +18,23 @@ public interface PostRepository extends CrudRepository<Post, Integer> {
     List<Post> findByCreatedGreaterThanEqualAndCreatedLessThanEqual(LocalDateTime startAt, LocalDateTime finishAt);
 
     Page<Post> findByOrderByCreated(LocalDateTime startAt, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            update Post p set p.title = :title, p.text = :text
+            where p.id = :id
+            """)
+    int updateTitleAndText(@Param("title") String title, @Param("text") String text, @Param("id") Integer id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            delete Image i where i.post.id  = :id
+            """)
+    int deleteImageByPostId(@Param("id") Integer id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            delete Post p where p.id  = :id
+            """)
+    int deleteByPostId(@Param("id") Integer id);
 }
--- src/main/java/ru/job4j/socialmediaapi/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/UserRepository.java
@@ -1,7 +1,46 @@
 package ru.job4j.socialmediaapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmediaapi.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("""
+            select user from User user
+            where user.name = :name
+            and user.password = :password
+            """)
+    Optional<User> findByNameAndPassword(@Param("name") String title, @Param("password") String password);
+
+    @Query("""
+            select user from User user
+            join Activity act
+            join Subscription sub
+            where sub.user.id = ?1
+            """)
+    List<User> findAllSubscribersById(int id);
+
+    @Query("""
+            select user from User user
+            join Friendship fr
+            where (fr.userFrom.id = ?1 or fr.userTo.id = ?1) and fr.status = true
+            """)
+    List<User> findAllFriendsById(int id);
+
+    @Query("""
+            select p from Post p
+            join User u
+            join Subscription sub
+            join Activity act
+            where act.user.id = ?1
+            order by p.created desc
+            """)
+    Page<User> findAllSubscriberPostsByOrderDescWithPagination(int id, Pageable pageable);
 }
